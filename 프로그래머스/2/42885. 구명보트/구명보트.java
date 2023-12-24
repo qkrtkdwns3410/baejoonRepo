4d9df0ec2d06dc726a044fc80ba6cea3c2353855
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2023-12-25 00:33:59 +0900

    [level 2] Title: 구명보트, Time: 8.30 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    
+    public int solution(int[] people, int limit) {
+        
+        Arrays.sort(people);
+        
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--; // 무거운 사람 탑승
+            boats++;
+        }
+        
+        return boats;
+    }
+}
