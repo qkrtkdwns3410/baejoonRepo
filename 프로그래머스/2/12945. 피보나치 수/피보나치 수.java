7c6b5b773722e933031fb734397039c14de93da5
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-03-31 19:32:39 +0900

    [level 2] Title: 피보나치 수, Time: 4.54 ms, Memory: 82 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int[] fibo = new int[100_001];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        
+        for(int i = 2 ; i <= n ; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        }
+        
+        
+        return fibo[n];
+    }
+}
